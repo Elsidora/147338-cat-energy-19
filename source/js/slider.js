53b42509ed348472f54460c07efe2f43e36017ff
ella <ellasidorova@yandex.ru>
2020-04-13 04:25:58 +0300

    slider

---
+++ source/js/slider.js
@@ -0,0 +1,19 @@
+'use strict';
+const sliderButtons = document.querySelector('.slider__buttons');
+const sliderItems = document.querySelectorAll('.slider__item');
+
+const onSliderButtonsClick = (evt) => {
+  evt.preventDefault();
+  const {target} = evt;
+  sliderItems.forEach( (sliderItem) => {
+    sliderItem.classList.add('slider-hidden');
+    target.blur();
+    if (sliderItem.id === target.value) {
+      sliderItem.classList.remove('slider-hidden');
+
+    }
+
+  });
+};
+
+sliderButtons.addEventListener('click', onSliderButtonsClick);
