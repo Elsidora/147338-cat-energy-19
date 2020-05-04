948bcd4b10d2cb51a2810aa8e98fcd3daab880b7
ella <ellasidorova@yandex.ru>
2020-05-04 23:32:45 +0300

    footer

--- source/js/map.js
+++ source/js/map.js
@@ -4,6 +4,20 @@ var init = function init() {
     center: [59.938635, 30.323118],
     zoom: 17
   });
+
+  myPlacemark = new ymaps.Placemark([59.938635, 30.323118], {
+    hintContent: 'Cat Energy',
+    balloonContent: 'Мы здесь'
+  },
+  {
+
+    iconLayout: 'default#image',
+    iconImageHref: 'img/map-pin.png',
+    iconImageSize: [55, 53],
+    iconImageOffset: [-27.5, -53]
+
+  }),
+  myMap.geoObjects.add(myPlacemark);
 };
 
 ymaps.ready(init);
